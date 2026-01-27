451df985d9f3ff4b42d89f4cd6517b3cf95cb3fa
Brielle Harrison <brielle_harrison@intuit.com>
2026-01-27 15:16:01 -0800

    Macro declaration also needs updating

--- Sources/ExtensibleEnumKit/MacroDeclaration.swift
+++ Sources/ExtensibleEnumKit/MacroDeclaration.swift
@@ -1,2 +1,2 @@
 @attached(member, names: named(RawValue), named(rawValue), named(init))
-public macro Extensible<T>(_ type: T.Type) = #externalMacro(module: "ExtensibleEnumMacros", type: "ExtensibleEnumMacro")
+public macro Extensible<T>(_ type: T.Type) = #externalMacro(module: "ExtensibleEnumMacros", type: "ExtensibleEnumerationMacro")
