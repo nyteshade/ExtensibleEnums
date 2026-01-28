c646261ec14f6c87281fc8912e887ff8ce3488fb
Brielle Harrison <brielle_harrison@intuit.com>
2026-01-27 16:00:30 -0800

    Compile, check, creep..................

--- Sources/ExtensibleEnumKit/ExtensibleEnum.swift
+++ Sources/ExtensibleEnumKit/ExtensibleEnum.swift
@@ -2,7 +2,7 @@ import Foundation
 
 @objc
 open class ExtensibleEnum: NSObject {
-  public let rawValue: Any
+  public var rawValue: Any
 
   @objc public nonisolated init?(rawValue: Any) {
     self.rawValue = rawValue
