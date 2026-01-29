64baef47d3efd7798b2b68528674ee881bb845e7
Brielle Harrison <nyteshade@gmail.com>
2026-01-28 21:03:38 -0800

    Adjusting the fit

--- Sources/ExtensibleEnumKit/ExtensibleEnum.swift
+++ Sources/ExtensibleEnumKit/ExtensibleEnum.swift
@@ -2,10 +2,10 @@ import Foundation
 
 @objc
 open class ExtensibleEnum: NSObject {
-  public var rawValue: Any
+  @objc public var rawValue: Any
 
-  public nonisolated init?(rawValue: Any) {
-    self.rawValue = rawValue
+  @objc override public nonisolated init() {
+    rawValue = NSNull()
     super.init()
   }
 }
